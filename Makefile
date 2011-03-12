8e3c1f5059cc53fdae42ad3c3f05b0fcd25ce23a
Stefan Karpinski <stefan@karpinski.org>
2011-03-11 19:13:05 -0500

    Implement basic encoding for integer and float data.

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 odb: odb.c
 	gcc -g3 -std=gnu99 $< -o $@
 
+test-encode:
+	./odb encode term:int correction:int p:float -- test.tsv > test.odb
+
 clean:
 	rm -rf odb odb.dSYM
 
-.PHONY: clean
+.PHONY: clean test-encode
--- odb.c
+++ odb.c
@@ -4,6 +4,11 @@
 #include <string.h>
 #include <stdarg.h>
 #include <getopt.h>
+#include <math.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/param.h>
+#include <sys/sysctl.h>
 
 #define errstr strerror(errno)
 
@@ -81,6 +86,15 @@ typedef enum {
     STRING
 } field_type_t;
 
+char *typestr(field_type_t type) {
+    switch (type) {
+        case INTEGER: return "int";
+        case FLOAT:   return "float";
+        case STRING:  return "string";
+        default:      return "unknown";
+    }
+}
+
 typedef struct {
     field_type_t type;
     char name[256-sizeof(field_type_t)];
@@ -102,32 +116,106 @@ field_spec_t parse_field_spec(const char *const str) {
     return spec;
 }
 
+char *get_line(FILE *file, char **buffer, size_t *len) {
+#if defined(__MACOSX__) || defined(__APPLE__)
+  *buffer = fgetln(file,len);
+  dieif(ferror(file), "error reading line: %s\n", errstr);
+  return *buffer;
+#else
+  int r = getline(buffer,len,file);
+  if (r != -1) return *buffer;
+  if (feof(file)) {
+    if (buffer) free(*buffer);
+    return *buffer = NULL;
+  } else if (ferror(file)) {
+    die("error reading line: %s\n", errstr);
+  }
+#endif
+  die("unknown get_line badness\n");
+}
+
+void fwriten(const void *restrict ptr, size_t size, size_t n, FILE *restrict stream) {
+    dieif(fwrite(ptr, size, n, stream) != n, "write error: %s\n", errstr);
+}
+
+void fwrite1(const void *restrict ptr, size_t size, FILE *restrict stream) {
+    fwriten(ptr, size, 1, stream);
+}
+
+static const char *const magic = "odb";
+static const unsigned long long bom = 0x0123456789abcdef;
+
 int main(int argc, char **argv) {
     parse_opts(&argc,&argv);
     dieif(argc < 1, "usage: %s\n", usage);
 
-    cmd_t cmd =
-        !strcmp(argv[0], "encode") ? ENCODE  :
-        !strcmp(argv[0], "decode") ? DECODE  :
-        !strcmp(argv[0], "help")   ? HELP    :
-                                     INVALID ;
+    cmd_t cmd = !strcmp(argv[0], "encode") ? ENCODE  :
+                !strcmp(argv[0], "decode") ? DECODE  :
+                !strcmp(argv[0], "help")   ? HELP    :
+                                             INVALID ;
     argv++; argc--;
 
     switch (cmd) {
         case ENCODE: {
-            int i;
+            fwrite1(magic, strlen(magic)+1, stdout);
+            fwrite1(&bom, sizeof(bom), stdout);
+
+            long long n;
             field_spec_t *specs = malloc(argc*sizeof(field_spec_t));
-            for (i = 0; i < argc; i++) {
-                if (!strcmp(argv[i], "--")) {
-                    specs = realloc(specs, i++);
+            for (n = 0; n < argc; n++) {
+                if (!strcmp(argv[n], "--")) {
+                    specs = realloc(specs, n++);
                     break;
                 }
-                specs[i] = parse_field_spec(argv[i]);
+                specs[n] = parse_field_spec(argv[n]);
             }
-            argv += i; argc -= i;
+            argv += n; argc -= n; n--;
 
-            for (i = 0; i < argc; i++)
-                warn("file: %s\n", argv[i]);
+            fwrite1(&n, sizeof(n), stdout);
+            fwriten(specs, sizeof(field_spec_t), n, stdout);
+
+            for (int i = 0; i < argc; i++) {
+                FILE *file = fopen(argv[i], "r");
+                dieif(!file, "error opening %s: %s\n", argv[i], errstr);
+                size_t length;
+                char *line, *buffer = NULL;
+                while (line = get_line(file,&buffer,&length)) {
+                    for (int j = 0; j < n; j++) {
+                        switch (specs[j].type) {
+                            case INTEGER: {
+                                errno = 0;
+                                long long v = strtoll(line,&line,10);
+                                dieif(errno == EINVAL && v == 0, "invalid integer at: %s", line);
+                                dieif(errno == ERANGE && v == LLONG_MIN, "integer underflow: %s", line);
+                                dieif(errno == ERANGE && v == LLONG_MAX, "integer overflow: %s", line);
+                                fwrite1(&v, sizeof(v), stdout);
+                                break;
+                            }
+                            case FLOAT: {
+                                char *p;
+                                errno = 0;
+                                double v = strtod(line,&p);
+                                dieif(p == line && v == 0.0, "invalid float at: %s", line);
+                                dieif(errno == ERANGE && v == 0.0, "float underflow: %s", line);
+                                dieif(errno == ERANGE && abs(v) == HUGE_VAL, "float overflow: %s", line);
+                                fwrite1(&v, sizeof(v), stdout);
+                                line = p;
+                                break;
+                            }
+                            default:
+                                die("encoding type %s not yet implemented\n", typestr(specs[j].type));
+                        }
+                        if (j < n-1) {
+                            dieif(line[0] != '\t', "tab expected, got '%c'", line[0]);
+                            line++;
+                        } else {
+                            dieif(!(line[0] == '\n' || line[0] == '\r'),
+                                  "end of line expected, got '%c'", line[0]);
+                        }
+                    }
+                }
+                dieif(fclose(file), "error closing %s: %s\n", argv[i], errstr);
+            }
 
             return 0;
         }
